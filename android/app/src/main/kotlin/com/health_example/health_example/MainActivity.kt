a9026b0e207736f87d8dd01a73f5e6e481a5ac7b
aliaouidet <alitunisi9@gmail.com>
2024-03-03 04:12:24 +0100

    The Commit message

---
+++ android/app/src/main/kotlin/com/health_example/health_example/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.health_example.health_example
+
+import android.os.Bundle
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
